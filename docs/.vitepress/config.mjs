abd94383b5b7facd5cd7c068f6e7131b17252df3
YYHSOfficial <141565155+YYHSOfficial@users.noreply.github.com>
2024-07-05 22:11:52 +0800

    1

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "郧阳中学食用指南",
+  base: '/YYHSEatingGuide/'
+  description: "郧中学子的校园内外百科书",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
