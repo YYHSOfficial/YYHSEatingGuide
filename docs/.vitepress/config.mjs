4ae32256eb08cbad24e5d98cd8c3ed1f315d02ea
YYHSOfficial <141565155+YYHSOfficial@users.noreply.github.com>
2024-07-06 21:20:06 +0800

    update files and add folders

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "郧阳中学食用指南",
+  base: '/YYHSEatingGuide/',
   description: "郧中学子的校园内外百科书",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -13,12 +14,41 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '初入校园',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '入校指南', link: '/freshman/when-u-start-ur-yyhs-journey' }
         ]
-      }
+      },
+      {
+        text: '日常生活',
+        items: [
+          { text: 'Coming Soon', link: '/dailylife/comingsoon' }
+        ]
+      },
+      {
+        text: '学习生活',
+        items: [
+          { text: 'Coming Soon', link: '/study/comingsoon' }
+        ]
+      },
+      {
+        text: '良师益友',
+        items: [
+          { text: 'Coming Soon', link: '/sensei/comingsoon' }
+        ]
+      },
+      {
+        text: '友情链接',
+        items: [
+          { text: 'Coming Soon', link: '/link/comingsoon' }
+        ]
+      },
+      {
+        text: '留言板',
+        items: [
+          { text: 'Coming Soon', link: '/comment/comingsoon' }
+        ]
+      },
     ],
 
     socialLinks: [
