6239722d89b439b54b6255ab90af8486ce56ac6b
YYHSOfficial <141565155+YYHSOfficial@users.noreply.github.com>
2024-07-08 18:35:14 +0800

    添加评论区

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,7 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
 
 import { inBrowser } from 'vitepress'
 import busuanzi from 'busuanzi.pure.js'
@@ -14,4 +16,28 @@ export default {
       }
     }
   },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = useData();
+    const route = useRoute();
+        
+    // giscus配置
+    giscusTalk({
+      repo: 'YYHSOfficial/YYHSEatingGuide', //仓库
+      repoId: 'R_kgDOMSZ-Og', //仓库ID
+      category: 'Announcements', // 讨论分类
+      categoryId: 'DIC_kwDOMSZ-Os4CgpKC', //讨论分类ID
+      mapping: 'pathname',
+      inputPosition: 'bottom',
+      lang: 'zh-CN',
+      }, 
+      {
+        frontmatter, route
+      },
+      //默认值为true，表示已启用，此参数可以忽略；
+      //如果为false，则表示未启用
+      //您可以使用“comment:true”序言在页面上单独启用它
+      true
+    );
+}
 }
